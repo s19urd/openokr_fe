71788bee85602fea9c2ea2fb718a2693cebfaeed
linaer <aelin@onylou.com>
2019-03-14 11:06:46 +0800

    json访问路径修改

--- okr-app/src/api/backend/resources/dailyWork.js
+++ okr-app/src/api/backend/resources/dailyWork.js
@@ -2,13 +2,13 @@ export default function (instance) {
   return {
     saveTask (taskArray) {
       return instance
-        .post('/api/daily/saveDailyList.json', taskArray)
+        .post('/okr-web/api/daily/saveDailyList.json', taskArray)
         .then(response => response.data)
     },
 
     queryTaskListByPage () {
       return instance
-        .post('/task/getTaskListByPage.json', {
+        .post('/okr-web/task/getTaskListByPage.json', {
           pageSize: 100
         })
     }
--- okr-app/src/api/backend/resources/login.js
+++ okr-app/src/api/backend/resources/login.js
@@ -2,14 +2,14 @@ export default function (instance) {
   return {
     saveTask (taskArray) {
       return instance
-        .post('/devLogin.json?username=admin', taskArray)
+        .post('/okr-web/devLogin.json?username=admin', taskArray)
         .then(response => response.data)
     },
 
     signIn (userInfo) {
       console.log(userInfo)
       return instance
-        .post('/devLogin.json', userInfo)
+        .post('/okr-web/devLogin.json', userInfo)
         .then(response => response.data)
     }
   }
